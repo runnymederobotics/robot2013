ca0d421ab140921ec31c975ad69251ae140100e6
Brian <Brian@192.168.1.127>
2013-03-10 17:35:42 -0700

    detect contours

--- src/dashboard/js/lib/smoothie.js
+++ src/dashboard/js/lib/smoothie.js
@@ -254,7 +254,7 @@ SmoothieChart.prototype.render = function(canvas, time) {
 
     // Set style for this dataSet.
     canvasContext.lineWidth = seriesOptions.lineWidth || 1;
-    canvasContext.fillStyle = seriesOptions.fillStyle;
+    canvasContext.fillStyle = seriesOptions.fillStyle || canvasContext.fillStyle;
     canvasContext.strokeStyle = seriesOptions.strokeStyle || '#ffffff';
     // Draw the line...
     canvasContext.beginPath();
