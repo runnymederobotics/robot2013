0ad016e0cef6b3640e05448a271d6c650255cae6
Brian Watling <brianwatling@hotmail.com>
2013-03-11 20:40:00 -0700

    Revert "fix cross origin for real this time"
    This reverts commit 5cc7d3c26d7cf947590d57cb120a8345e2fd68a7.

--- src/dashboard/camera.js
+++ src/dashboard/camera.js
@@ -72,7 +72,7 @@ var startCamera = function(divId, url, callback) {
         context.lineWidth = oldWidth;
     }
             
-    var image = imageDiv.append("<img src='#' crossOrigin='Anonymous'/>").children("img");
+    var image = imageDiv.append("<img src='#' cross-origin:'anonymous'/>").children("img");
     image.load(function() {
         if (!context) {
             context = getCanvasContext(this.width, this.height, imageDiv);
@@ -133,7 +133,9 @@ var startCamera = function(divId, url, callback) {
         if (now - lastImageTime > 2000) {
             connectionCount += 1;
             connectionCounter.text("Connection Attempts: " + connectionCount);
-            image.attr("src", "#");
+            //image.attr("src", "#");
+            //image.attr("width", 320);
+            //image.attr("height", 240);
             image.attr("src", url);
         } else {
             connectionCounter.text("");
