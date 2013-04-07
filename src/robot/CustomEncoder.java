6c5674f4e19b74aa5e39b7909db9f3a6e428eb45
Braden Watling <bradenwatling@hotmail.com>
2013-04-07 01:33:53 -0400

    Made a change to WPILib to make m_counter public in Counter.java
    To recompile WPILib: extract sunspotfrcsdk/lib/wpilibj.project.zip
    Open the extracted project in NetBeans
    Change "private tCounter m_counter;" in Counter.java to be public
    Compile and copy the contents of dist/lib to subspotfrcsdk/lib

--- src/robot/CustomEncoder.java
+++ src/robot/CustomEncoder.java
@@ -4,14 +4,25 @@ import edu.wpi.first.wpilibj.Counter;
 import edu.wpi.first.wpilibj.PIDSource;
 
 public class CustomEncoder extends Counter implements PIDSource {
-
+    
     public CustomEncoder(int channel) {
         super(channel);
+
+        //This requires a change in WPILib to make m_counter public
+        //To recompile WPILib: extract sunspotfrcsdk/lib/wpilibj.project.zip
+        //Open the extracted project in NetBeans
+        //Change "private tCounter m_counter;" in Counter.java to be public
+        //Compile and copy the contents of dist/lib to subspotfrcsdk/lib
+        
+        //Using this creates a ring buffer that is averaged to get a more
+        //accurate reading
+        //Don't put more than 127
+        this.m_counter.writeTimerConfig_AverageSize(100);
     }
 
     public double pidGet() {
         double period = super.getPeriod();
         
-        return 1.0 / ((period == 0) ? 1.0 : super.getPeriod());
+        return 1.0 / ((period == 0) ? 1.0 : period);
     }
 }
